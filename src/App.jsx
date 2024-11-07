c9523ce4011a782ca3f605099584cdc186d514cd
heart <150511179+Nuelsti@users.noreply.github.com>
2024-11-07 09:03:32 +0100

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RichTextEditor from './components/richtexteditor'
+
+function App() {
+  return (
+    <>
+      <RichTextEditor />
+    </>
+
+  )
+}
+
+export default App
